740228d60ed786e4793b18141490d1a936e640a9
Luca Burgazzoli <lburgazzoli@gmail.com>
2024-03-21 09:15:06 +0100

    chore(assistant-backend): improve config and client

--- assistant-backend/src/main/java/org/apache/camel/assistant/web/resources/AssistantProducers.java
+++ assistant-backend/src/main/java/org/apache/camel/assistant/web/resources/AssistantProducers.java
@@ -1,4 +1,20 @@
 package org.apache.camel.assistant.web.resources;
 
+import io.qdrant.client.QdrantClient;
+import io.qdrant.client.QdrantGrpcClient;
+import jakarta.enterprise.context.ApplicationScoped;
+import jakarta.enterprise.inject.Produces;
+
+@ApplicationScoped
 public class AssistantProducers {
+    @Produces
+    public QdrantClient qdrantClient(AssistantConfiguration configuration) {
+        QdrantGrpcClient gc = QdrantGrpcClient.newBuilder(
+                configuration.qdrant().host(),
+                configuration.qdrant().grpcPort(),
+                false)
+            .build();
+
+        return new QdrantClient(gc);
+    }
 }
