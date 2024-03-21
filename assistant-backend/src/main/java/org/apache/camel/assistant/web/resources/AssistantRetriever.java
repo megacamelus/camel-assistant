79a0c8d6c2ea69c5fea273f85233f32cf7bef567
Luca Burgazzoli <lburgazzoli@gmail.com>
2024-03-21 09:03:02 +0100

    chore(assistant-backend): fix deprecations

---
+++ assistant-backend/src/main/java/org/apache/camel/assistant/web/resources/AssistantRetriever.java
@@ -0,0 +1,34 @@
+package org.apache.camel.assistant.web.resources;
+
+import java.util.function.Supplier;
+
+import dev.langchain4j.model.embedding.AllMiniLmL6V2EmbeddingModel;
+import dev.langchain4j.rag.DefaultRetrievalAugmentor;
+import dev.langchain4j.rag.RetrievalAugmentor;
+import dev.langchain4j.rag.content.retriever.EmbeddingStoreContentRetriever;
+import dev.langchain4j.store.embedding.qdrant.QdrantEmbeddingStore;
+import jakarta.inject.Singleton;
+
+@Singleton
+public class AssistantRetriever implements Supplier<RetrievalAugmentor> {
+    private final RetrievalAugmentor augmentor;
+
+    AssistantRetriever(QdrantEmbeddingStore store, AllMiniLmL6V2EmbeddingModel model) {
+        EmbeddingStoreContentRetriever contentRetriever = EmbeddingStoreContentRetriever.builder()
+            .embeddingModel(model)
+            .embeddingStore(store)
+            .maxResults(3)
+            .build();
+
+        augmentor = DefaultRetrievalAugmentor
+            .builder()
+            .contentRetriever(contentRetriever)
+            .build();
+    }
+
+    @Override
+    public RetrievalAugmentor get() {
+        return augmentor;
+    }
+
+}
