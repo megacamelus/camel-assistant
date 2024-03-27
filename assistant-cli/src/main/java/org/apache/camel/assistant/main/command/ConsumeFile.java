3f2fc62dd0d1fcc21171c5bc943b50445f8d99ba
Otavio Rodolfo Piske <angusyoung@gmail.com>
2024-03-27 15:55:46 +0100

    Added support for consuming PDFs

---
+++ assistant-cli/src/main/java/org/apache/camel/assistant/main/command/ConsumeFile.java
@@ -0,0 +1,71 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.apache.camel.assistant.main.command;
+
+import java.io.BufferedInputStream;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.URI;
+
+import io.quarkus.rest.client.reactive.QuarkusRestClientBuilder;
+import org.apache.camel.assistant.main.consume.ConsumeService;
+import picocli.CommandLine;
+
+@CommandLine.Command(name = "file",
+        description = "Consume a new dynamic or static knowledge", sortOptions = false)
+public class ConsumeFile extends BaseCommand {
+
+    @CommandLine.Option(names = {"-d", "--dynamic"}, description = "Whether it is using static or dynamic information")
+    private boolean dynamic;
+
+    @CommandLine.Option(names = {"--source"}, description = "The source of information", arity = "0..1")
+    private String source;
+
+    @CommandLine.Option(names = {"--id"}, description = "The ID of the information from the source", arity = "0..1")
+    private String id;
+
+    // TODO: needs to support more than one file per invocation
+    @CommandLine.Parameters(description = "The file to load", arity = "1..1")
+    private String file;
+
+    @CommandLine.Option(names = {"--address"}, description = "The service address", arity = "0..1", required = true, defaultValue = "http://localhost:8083")
+    private String address;
+
+    ConsumeService learnService;
+
+    @Override
+    public void run() {
+        learnService = QuarkusRestClientBuilder.newBuilder()
+                .baseUri(URI.create(address))
+                .build(ConsumeService.class);
+
+        try (InputStream stream = new BufferedInputStream(new FileInputStream(file))) {
+            final byte[] pdfBytes = stream.readAllBytes();
+
+            learnService.consumePdfStatic(pdfBytes);
+        } catch (FileNotFoundException e) {
+            System.err.printf("Cannot process file %s: file does not exist%n", file);
+            System.exit(1);
+        } catch (IOException e) {
+            System.err.printf("Unhandled I/O error processing file %s: %s%n", file, e.getMessage());
+            System.exit(1);
+        }
+    }
+}
--- assistant-ingestion-sources/plain-text-source/src/main/java/org/apache/camel/assistant/data/ingestion/source/plaintext/PlainTextRoute.java
+++ assistant-ingestion-sources/plain-text-source/src/main/java/org/apache/camel/assistant/data/ingestion/source/plaintext/PlainTextRoute.java
@@ -17,29 +17,108 @@
 
 package org.apache.camel.assistant.data.ingestion.source.plaintext;
 
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.apache.camel.Exchange;
 import org.apache.camel.builder.RouteBuilder;
+import org.apache.pdfbox.Loader;
+import org.apache.pdfbox.pdmodel.PDDocument;
+import org.jboss.logging.Logger;
 
 public class PlainTextRoute extends RouteBuilder {
+    private static final Logger LOG = Logger.getLogger(PlainTextRoute.class);
+
+    private static final String DATA_SIZE = "DATA_SIZE";
+
+    // By default, Kafka won't accept payloads greater than 1048576,
+    // give the additional data that may transit, we limit it to a much
+    // lower value
+    private static final int MAX_DATA_SIZE = 1000000;
+
+    // AI APIs have an upper limit to the amount of tokens
+    // they can accept. Typically this is 4096 ...
+    // but given the context, question and everything else, we
+    // use a much lower number
+    private static final int MAX_TOKENS = 384;
+
+    private void filter(Exchange exchange) {
+        String unfilteredData = exchange.getMessage().getBody(String.class);
+
+        exchange.getMessage().setBody(unfilteredData.replace('\n', ' '));
+    }
+
+    private void convertBytesToPDFFile(Exchange e) throws IOException {
+        final byte[] body = e.getMessage().getBody(byte[].class);
+
+        // TODO: Camel should probably do this itself
+        PDDocument document = Loader.loadPDF(body);
+
+        e.getMessage().setHeader(DATA_SIZE, body.length);
+        e.getMessage().setBody(document);
+    }
+
+    public static class CustomSplitter {
+        // TODO: this is very heavy and slow ... optimize this. Also, this is very
+        //  poor at providing context.
+
+        public List<String> largeDataSplitter(Exchange exchange) {
+            final String body = exchange.getMessage().getBody(String.class);
+            final String[] parts = body.split(" ");
+
+            List<String> strings = new ArrayList<>();
+
+            for (int i = 0; i < parts.length; i++) {
+                StringBuffer sb = new StringBuffer();
+
+                int r = 0;
+                for (; r < MAX_TOKENS && r + i < parts.length && sb.length() < MAX_DATA_SIZE; r++) {
+                    sb.append(parts[i + r]);
+                    sb.append(" ");
+                }
+                i += r;
+
+                strings.add(sb.toString());
+            }
+
+            LOG.infof("Finished splitting: %d parts", strings.size());
+            return strings;
+        }
+    }
+
+
 
     @Override
     public void configure() throws Exception {
         rest("/api")
                 .get("/hello").to("direct:hello")
-                .post("/consume/static").to("direct:consumeStatic")
-                .post("/consume/dynamic/{source}/{id}").to("direct:consumeDynamic");
+                .post("/consume/text/static").to("direct:consumeTextStatic")
+                .post("/consume/text/dynamic/{source}/{id}").consumes("application/octet-stream").to("direct:consumeTextDynamic")
+                .post("/consume/pdf/static").to("direct:consumePdfStatic");
 
         from("direct:hello")
                 .routeId("source-web-hello")
                 .transform().constant("Hello World");
 
-        from("direct:consumeDynamic")
-                .routeId("source-consume-dynamic-route")
+
+        from("direct:consumePdfStatic")
+                .routeId("source-consume-pdf-static-route")
+                .process(this::convertBytesToPDFFile)
+                .pipeline()
+                    .to("pdf:extractText")
+                    .process(this::filter)
+                    .split(method(new CustomSplitter(), "largeDataSplitter")).streaming()
+                    .to("kafka:ingestion?brokers={{bootstrap.servers}}");
+
+        from("direct:consumeTextDynamic")
+                .routeId("source-consume-text-dynamic-route")
                 .setHeader("dynamic", constant("true"))
                 .to("kafka:ingestion?brokers={{bootstrap.servers}}")
                 .transform().constant("Dynamic data loaded");
 
-        from("direct:consumeStatic")
-                .routeId("source-consume-static-route")
+        from("direct:consumeTextStatic")
+                .routeId("source-consume-text-static-route")
                 .to("kafka:ingestion?brokers={{bootstrap.servers}}")
                 .transform().constant("Static data loaded");
     }
