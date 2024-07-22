1102d25ece70a03690e2127123520d1caff734b7
Otavio Rodolfo Piske <angusyoung@gmail.com>
2024-07-22 18:38:15 +0200

    Fix NPE when setting up the retriever

--- assistant-backend/src/main/java/org/apache/camel/assistant/web/resources/AssistantRetriever.java
+++ assistant-backend/src/main/java/org/apache/camel/assistant/web/resources/AssistantRetriever.java
@@ -2,23 +2,19 @@ package org.apache.camel.assistant.web.resources;
 
 import java.util.function.Supplier;
 
+import jakarta.inject.Singleton;
+
 import dev.langchain4j.model.embedding.AllMiniLmL6V2EmbeddingModel;
 import dev.langchain4j.rag.DefaultRetrievalAugmentor;
 import dev.langchain4j.rag.RetrievalAugmentor;
 import dev.langchain4j.rag.content.retriever.EmbeddingStoreContentRetriever;
 import dev.langchain4j.store.embedding.qdrant.QdrantEmbeddingStore;
 
-import jakarta.inject.Inject;
-import jakarta.inject.Singleton;
-
 @Singleton
 public class AssistantRetriever implements Supplier<RetrievalAugmentor> {
-    @Inject
-    AssistantConfiguration configuration;
-
     private final RetrievalAugmentor augmentor;
 
-    AssistantRetriever(QdrantEmbeddingStore store, AllMiniLmL6V2EmbeddingModel model) {
+    AssistantRetriever(QdrantEmbeddingStore store, AllMiniLmL6V2EmbeddingModel model, AssistantConfiguration configuration) {
         EmbeddingStoreContentRetriever contentRetriever = EmbeddingStoreContentRetriever.builder()
             .embeddingModel(model)
             .embeddingStore(store)
