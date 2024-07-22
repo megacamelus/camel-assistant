d1ec3d8fb385745748c845adac09059a915d0982
Otavio Rodolfo Piske <angusyoung@gmail.com>
2024-07-22 17:52:25 +0200

    Allow configuring RAG parameters

--- assistant-backend/src/main/java/org/apache/camel/assistant/web/resources/AssistantRetriever.java
+++ assistant-backend/src/main/java/org/apache/camel/assistant/web/resources/AssistantRetriever.java
@@ -7,17 +7,23 @@ import dev.langchain4j.rag.DefaultRetrievalAugmentor;
 import dev.langchain4j.rag.RetrievalAugmentor;
 import dev.langchain4j.rag.content.retriever.EmbeddingStoreContentRetriever;
 import dev.langchain4j.store.embedding.qdrant.QdrantEmbeddingStore;
+
+import jakarta.inject.Inject;
 import jakarta.inject.Singleton;
 
 @Singleton
 public class AssistantRetriever implements Supplier<RetrievalAugmentor> {
+    @Inject
+    AssistantConfiguration configuration;
+
     private final RetrievalAugmentor augmentor;
 
     AssistantRetriever(QdrantEmbeddingStore store, AllMiniLmL6V2EmbeddingModel model) {
         EmbeddingStoreContentRetriever contentRetriever = EmbeddingStoreContentRetriever.builder()
             .embeddingModel(model)
             .embeddingStore(store)
-            .maxResults(3)
+            .maxResults(configuration.retriever().maxResults())
+            .minScore(configuration.retriever().minScore())
             .build();
 
         augmentor = DefaultRetrievalAugmentor
